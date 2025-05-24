eac3d701b60be2cbb74cf7b0478984ddebb96d98
Azimjonov Islomjon <azimjonovislomjon77@gmail.com>
2025-05-25 02:38:27 +0500

    weather

--- weather/api_urls.py
+++ weather/api_urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .api_views import WeatherAPIView, UserSearchHistoryList, CityStatsList
 
+app_name = 'weather_api'
+
 urlpatterns = [
     path('weather/', WeatherAPIView.as_view(), name='weather'),
     path('history/', UserSearchHistoryList.as_view(), name='search-history'),
